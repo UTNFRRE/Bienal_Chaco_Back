81999a5c8618d8bf5e8adb04d1ca77b29ef8be25
MartinLSME08 <martinlopezsoto@gmail.com>
2024-11-10 21:49:37 -0300

    * Agregado Flunt Validation * Reglas de Ediciones (años entre 2020/25; fecha fin superior a fecha inicio; evento no puede durar más de un mes)

---
+++ BackEnd-solucion/APIController/Validadores/EdicionValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Models;
+using Servicios;
+
+namespace APIController.Validadores
+{
+    public class EdicionValidator : AbstractValidator<Edicion>
+    {
+        public EdicionValidator()
+        {
+            RuleFor(edicion => edicion.Año).Must(AñoValido).WithMessage("Solo se admiten años entre 2020 y 2025");
+
+            RuleFor(edicion => edicion.FechaFin).Must((edicion, fechaFin) => fechaFin > edicion.FechaFin).WithMessage("La fecha de fin debe ser superior a fecha inicio")
+                                                .Must((edicion, fechaFin) => ValidarDuracion(edicion.FechaInicio, fechaFin)).WithMessage("Un evento no puede durar más de un mes");
+            //si hay una fecha de Inicio, no puede no haber una fecha de fin
+
+            //si hay una fecha de fin, no puede no haber una fecha de inicio
+        }
+
+        public bool AñoValido(int año)
+        {
+            return año >= 2020 && año <= 2025;
+        }
+
+        public bool FechaFinValido(DateOnly fechaFin, DateOnly fechaInicio)
+        {
+            return fechaFin > fechaInicio;
+        }
+
+        public bool ValidarDuracion(DateOnly fechaIncio, DateOnly fechaFin)
+        {
+            if (fechaFin.CompareTo(fechaIncio) <= 30)
+                { 
+                return true;
+                }
+            else
+                  {
+                return false;
+            }
+            
+        }
+    }
+}
